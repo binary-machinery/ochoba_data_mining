3b838a6fec81e37d85b1de3fcf36323fd4183cfc
Evgeniy Prikhodko <fck.r.sns@gmail.com>
2020-06-04 23:23:42 +0300

    Get posts

--- get_posts.py
+++ get_posts.py
@@ -7,11 +7,11 @@ import psycopg2
 import requests
 
 
-class GetUsers:
+class GetPosts:
     class Stats:
         def __init__(self):
             self.request_count = 0
-            self.user_count = 0
+            self.post_count = 0
             self.error_count = 0
             self.requests_since_last_429 = 0
 
@@ -20,7 +20,7 @@ class GetUsers:
         with open(str(script_path) + "/config.json") as json_file:
             config = json.load(json_file)
 
-        self.api_user_url = config["api"]["url"] + "user/"
+        self.api_post_url = config["api"]["url"] + "entry/"
         self.api_headers = {"X-Device-Token": config["api"]["token"]}
         self.conn = psycopg2.connect(host=config["db"]["host"],
                                      port=config["db"]["port"],
@@ -34,22 +34,22 @@ class GetUsers:
         self.cursor.close()
         self.conn.close()
 
-    def get_users(self):
+    def get_posts(self):
         print("Started at " + datetime.now().strftime("%H:%M:%S"))
-        for user_id in range(1, 250000):
-            self.__get_user(user_id)
+        for user_id in range(141749, 141750):
+            self.__get_post(user_id)
             self.conn.commit()
 
-    def __get_user(self, user_id):
+    def __get_post(self, post_id):
         if self.stats.request_count % 100 == 0:
             self.conn.commit()
-            print("{0}: {1} requests processed ({2} users, {3} errors)"
+            print("{0}: {1} requests processed ({2} posts, {3} errors)"
                   .format(datetime.now().strftime("%H:%M:%S"),
                           self.stats.request_count,
-                          self.stats.user_count,
+                          self.stats.post_count,
                           self.stats.error_count))
 
-        response = requests.get(self.api_user_url + str(user_id), headers=self.api_headers)
+        response = requests.get(self.api_post_url + str(post_id), headers=self.api_headers)
         if response.status_code == 429:
             # Too Many Requests
             print(datetime.now().strftime("%H:%M:%S")
@@ -58,7 +58,7 @@ class GetUsers:
                   + ". Wait for 60 seconds and repeat")
             self.stats.requests_since_last_429 = 0
             time.sleep(60)
-            self.__get_user(user_id)
+            self.__get_post(post_id)
             return
 
         response_json = response.json()
@@ -67,28 +67,41 @@ class GetUsers:
         if "error" in response_json:
             self.cursor.execute(
                 """
-                    insert into errors (user_id, response)
+                    insert into post_errors (post_id, response)
                         values (%s, %s);
                 """,
-                (user_id, str(response_json))
+                (post_id, str(response_json))
             )
             self.stats.error_count += 1
 
         else:
+            result = response_json["result"]
+            result.pop("entryContent", None)  # drop raw html content
+
+            text_length = 0
+            media_count = 0
+            if "blocks" in result:
+                for block in result["blocks"]:
+                    if block["type"] in ["media", "video"]:
+                        media_count += 1
+                    elif block["type"] in ["text", "header"]:
+                        text_length += len(block["data"]["text"])
+                del result["blocks"]
+
             self.cursor.execute(
                 """
-                    insert into users (id, json)
-                        values (%s, %s)
+                    insert into posts (id, json, text_length, media_count)
+                        values (%s, %s, %s, %s)
                     on conflict (id)
                         do update set json = excluded.json;
                 """,
-                (user_id, str(response_json))
+                (post_id, json.dumps(response_json), text_length, media_count)
             )
-            self.stats.user_count += 1
+            self.stats.post_count += 1
 
         self.stats.request_count += 1
         self.stats.requests_since_last_429 += 1
 
 
 if __name__ == "__main__":
-    GetUsers().get_users()
+    GetPosts().get_posts()
---
+++ sql/create_tables.sql
@@ -0,0 +1,23 @@
+create table users (
+    id int primary key,
+    json varchar not null
+);
+
+create table errors (
+    id serial primary key,
+    user_id int not null,
+    response varchar not null
+);
+
+create table posts (
+    id int primary key,
+    json varchar not null,
+    text_length int not null default 0,
+    media_count int not null default 0
+);
+
+create table post_errors (
+    id serial primary key,
+    post_id int not null,
+    response varchar not null
+);
