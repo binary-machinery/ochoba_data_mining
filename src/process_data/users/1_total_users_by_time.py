fd84a7b0aff5f1ef159f6b6d26c87ee5255fca85
LeLuCh <alekx@alumni.nottingham.ac.uk>
2020-07-03 16:45:27 +1000

    Update 1_total_users_by_time.py

--- src/process_data/users/1_total_users_by_time.py
+++ src/process_data/users/1_total_users_by_time.py
@@ -1,19 +1,23 @@
 from src.common.sql_plot import SqlPlot
 
 SqlPlot().show(
-    sql_query="""
-        with data as (
-            select date_trunc('day', created) as time_window,
-                   count(*) as new_users
-            from users
-            group by time_window
-        )
-        select time_window,
-                sum(new_users) over (
-                    order by time_window rows between unbounded preceding and current row
-                ) as total_users
-        from data
-    """,
+    sql_querys=[
+        {
+            'query': """
+                with data as (
+                    select date_trunc('day', created) as time_window,
+                           count(*) as new_users
+                    from users
+                    group by time_window
+                )
+                select time_window,
+                        sum(new_users) over (
+                            order by time_window rows between unbounded preceding and current row
+                        ) as total_users
+                from data
+            """, 'label': 'Пользователи'
+        }
+    ],
     title="Количество пользователей",
     x_label="Время",
     y_label="Пользователи"
