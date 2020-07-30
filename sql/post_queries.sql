bced1fe58831f61c2096439bbb00f2c2976b3429
Evgeniy Prikhodko <fck.r.sns@gmail.com>
2020-07-30 21:44:26 +0300

    Hits and percentiles

--- sql/post_queries.sql
+++ sql/post_queries.sql
@@ -65,3 +65,123 @@ update post_errors
         select to_timestamp(avg(cast(extract(epoch from created) as integer))) as created
         from data
     );
+
+-- Sort posts by hits
+select id, title, author_name, subsite_name, hits_count, likes_sum from posts
+where type = 1
+order by hits_count desc;
+
+-- Sort subsites by hits
+with data as (
+    select id,
+           created,
+           case when subsite_type = 1 then 0 else subsite_id end         as subsite_id,
+           case when subsite_type = 1 then 'Блоги' else subsite_name end as subsite_name,
+           subsite_type,
+           hits_count
+    from posts
+    where type = 1
+)
+select subsite_id, subsite_name, sum(hits_count) as hits from data
+group by subsite_id, subsite_name
+order by hits desc;
+
+-- Sort subsites by hits in June 2020 and show their hits ratio
+with data as (
+    select id,
+           created,
+           case when subsite_type = 1 then 0 else subsite_id end         as subsite_id,
+           case when subsite_type = 1 then 'Блоги' else subsite_name end as subsite_name,
+           subsite_type,
+           hits_count
+    from posts
+    where type = 1
+)
+select subsite_id, subsite_name, sum(hits_count) as hits,
+       concat(round(sum(hits_count)::decimal / (select sum(hits_count) from posts where created between '2020-06-01' and '2020-07-01') * 100, 2), '%') as ratio
+from data
+where created between '2020-06-01' and '2020-07-01'
+group by subsite_id, subsite_name
+order by hits desc;
+
+-- Sort posts by rating
+select id, title, author_name, subsite_name, hits_count, likes_sum from posts
+where type = 1
+order by likes_sum desc;
+
+-- Sort subsites by rating
+with data as (
+    select id,
+           created,
+           case when subsite_type = 1 then 0 else subsite_id end         as subsite_id,
+           case when subsite_type = 1 then 'Блоги' else subsite_name end as subsite_name,
+           subsite_type,
+           likes_sum
+    from posts
+    where type = 1
+)
+select subsite_id, subsite_name, sum(likes_sum) as rating from data
+group by subsite_id, subsite_name
+order by rating desc;
+
+-- Sort posts by negative rating
+select id, title, author_name, subsite_name, hits_count, likes_sum from posts
+where type = 1
+order by likes_sum;
+
+-- Sort posts by comments
+select id, title, author_name, subsite_name, hits_count, likes_sum, comments_count from posts
+where type = 1
+order by comments_count desc;
+
+-- Sort subsites by rating
+with data as (
+    select id,
+           created,
+           case when subsite_type = 1 then 0 else subsite_id end         as subsite_id,
+           case when subsite_type = 1 then 'Блоги' else subsite_name end as subsite_name,
+           subsite_type,
+           comments_count
+    from posts
+    where type = 1
+)
+select subsite_id, subsite_name, sum(comments_count) as comments from data
+group by subsite_id, subsite_name
+order by comments desc;
+
+-- Percentiles
+with length_data as (
+    select
+           posts.id as post_id,
+           sum(coalesce(blocks.text_length, 0)) as text_length
+    from posts
+    left join post_blocks blocks
+        on posts.id = blocks.post_id
+            and posts.type = 1
+    group by posts.id
+), percentiles as (
+    select generate_series as value
+    from generate_series(0, 1, 0.05)
+)
+select percentiles.value, percentile_disc(percentiles.value) within group (order by text_length)
+from percentiles cross join length_data
+group by percentiles.value;
+
+-- Long posts percentiles
+with length_data as (
+    select posts.id, sum(blocks.text_length) as text_length
+    from posts
+    join post_tags tags
+        on posts.id = tags.post_id
+            and posts.type = 1
+            and (value = '#лонг' or value = '#лонгрид')
+    join post_blocks blocks
+        on posts.id = blocks.post_id
+    group by posts.id
+), percentiles as (
+    select generate_series as value
+    from generate_series(0, 1, 0.05)
+)
+select percentiles.value, percentile_disc(percentiles.value) within group (order by text_length)
+from percentiles cross join length_data
+group by percentiles.value;
---
+++ src/process_data/posts/6_hits_by_time_by_subsite_normalized.py
@@ -0,0 +1,92 @@
+import plotly.graph_objects as go
+
+from src.common.config_loader import ConfigLoader
+from src.common.data_base_wrapper import DataBaseWrapper
+
+# https://plotly.com/python/filled-area-plots/
+
+config = ConfigLoader.load()
+db = DataBaseWrapper(config["db"])
+
+fig = go.Figure()
+
+subsite_ids, subsite_names = db.fetch_data(
+    """
+        select subsite_id, subsite_name
+        from posts
+        where type = 1 and subsite_type = 2
+        group by subsite_id, subsite_name
+        order by sum(hits_count) desc
+        limit 19
+    """,
+    None
+)
+
+for (subsite_id, subsite_name) in zip(subsite_ids, subsite_names):
+    x, y = db.fetch_data(
+        """
+            with time_scale as (
+                select date_trunc('week', generate_series) as time_window
+                from generate_series('2018-06-06'::timestamp, '2020-07-20'::timestamp, '1 week'::interval)
+                order by time_window
+            )
+            select time_scale.time_window, coalesce(data.sum, 0) as count
+            from time_scale
+            left join (
+                select date_trunc('week', created) as time_window, sum(hits_count)
+                from posts
+                where type = 1 and subsite_id = %s
+                group by time_window
+                order by time_window
+                ) as data
+                on time_scale.time_window = data.time_window
+        """,
+        (subsite_id,)
+    )
+    fig.add_trace(go.Scatter(
+        x=x,
+        y=y,
+        mode='lines',
+        stackgroup='one',
+        groupnorm='percent',
+        name=subsite_name
+    ))
+
+x, y = db.fetch_data(
+    """
+        with time_scale as (
+            select date_trunc('week', generate_series) as time_window
+            from generate_series('2018-06-06'::timestamp, '2020-07-20'::timestamp, '1 week'::interval)
+            order by time_window
+        )
+        select time_scale.time_window, coalesce(data.sum, 0) as count
+        from time_scale
+        left join (
+            select date_trunc('week', created) as time_window, sum(hits_count)
+            from posts
+            where type = 1 and subsite_type = 1
+            group by time_window
+            order by time_window
+            ) as data
+            on time_scale.time_window = data.time_window
+    """,
+    None
+)
+fig.add_trace(go.Scatter(
+    x=x,
+    y=y,
+    mode='lines',
+    stackgroup='one',
+    groupnorm='percent',
+    name='Блоги'
+))
+
+fig.update_layout(
+    showlegend=True,
+    xaxis_type='category',
+    yaxis=dict(
+        type='linear',
+        range=[1, 100],
+        ticksuffix='%'))
+
+fig.show()
